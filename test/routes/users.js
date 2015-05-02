dacd9c570b7313e182dfb1d49b200369758e203a
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-05-01 23:07:51 -0300

    add users test

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,17 +1,73 @@
 var expect = require('chai').expect;
 var request = require('supertest');
 var app = require("../../app");
-
-process.env.NODE_ENV = "test";
+var models = require("../../models");
 
 describe('User root routes', function(){
 
-	describe("get", function(){
+	var company;
+	var user;
+
+	before(function(done){
+		
+		models.Company.create({
+			name: "Aion"
+		}).then(function(_company){
+			company = _company;
+			var _userB =  models.User.build({
+				first_name: "Flora",
+		    last_name: "Dorea",
+		    email: "test@test.com",
+		    role: "manager",
+		    password: "test"
+			});
+			company.addUser(_userB).then(function(_user){
+				user = _user;
+				done();
+			});
+		});
+
+	});
+
+	after(function(done){
+		user.destroy().then(function(){
+			company.destroy().then(function(){
+				done();
+			});
+		});
+	});
+
+	context("get", function(){
 
 		it("gets users list", function(done){
 			request(app)
 				.get("/api/v1/users")
+				.set("App-Access-Token", user.access_token)
 				.expect(200, done);
+		});
+
+		it("doesnt access with wrong creds", function(done){
+			request(app)
+				.get("/api/v1/users")
+				.set("App-Access-Token", "blablls")
+				.expect(404, done);
+		});
+
+	});
+
+	context("post", function(){
+
+		it("creates an user", function(done){
+			request(app)
+				.post("/api/v1/users")
+				.set("App-Access-Token", user.access_token)
+				.send({
+					first_name: "Flora",
+			    last_name: "Dorea",
+			    email: "test@test.com",
+			    role: "manager",
+			    password: "test"
+				}).expect(201, done);
 		});
 
 	});

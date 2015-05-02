a450b42735267763a8a0f06e09de5112872088cd
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-05-02 20:31:36 -0300

    refactor users tests

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,73 +1,101 @@
 var expect = require('chai').expect;
-var request = require('supertest');
 var app = require("../../app");
 var models = require("../../models");
+var request = require('supertest')(app);
+
+function testNoAuthenticetedUser(url, method){
+	switch(method){
+		case "get": 
+			it("return 404 mith no access_token", function(done){
+				request
+					.get(url)
+					.expect(404, done);
+			});
+			break;
+		case "post":
+			it("return 404 mith no access_token", function(done){
+				request
+					.post(url)
+					.expect(404, done);
+			});
+			break;
+		case "patch":
+			it("return 404 mith no access_token", function(done){
+				request
+					.patch(url)
+					.expect(404, done);
+			});
+			break;
+		case "delete":
+			it("return 404 mith no access_token", function(done){
+				request
+					.delete(url)
+					.expect(404, done);
+			});
+			break;
+	}
+}
 
 describe('User root routes', function(){
 
 	var company;
 	var user;
 
+	var managerUser = {
+		first_name: "Flora",
+    last_name: "Dorea",
+    email: "manager@test.com",
+    role: "manager",
+    password: "test"
+	};
+
 	before(function(done){
-		
-		models.Company.create({
-			name: "Aion"
-		}).then(function(_company){
-			company = _company;
-			var _userB =  models.User.build({
-				first_name: "Flora",
-		    last_name: "Dorea",
-		    email: "test@test.com",
-		    role: "manager",
-		    password: "test"
-			});
-			company.addUser(_userB).then(function(_user){
-				user = _user;
-				done();
-			});
-		});
 
-	});
+		models.sequelize.sync({force: true, logging: false}).then(function(){
 
-	after(function(done){
-		user.destroy().then(function(){
-			company.destroy().then(function(){
-				done();
-			});
-		});
-	});
-
-	context("get", function(){
-
-		it("gets users list", function(done){
-			request(app)
-				.get("/api/v1/users")
-				.set("App-Access-Token", user.access_token)
-				.expect(200, done);
-		});
-
-		it("doesnt access with wrong creds", function(done){
-			request(app)
-				.get("/api/v1/users")
-				.set("App-Access-Token", "blablls")
-				.expect(404, done);
-		});
-
-	});
-
-	context("post", function(){
-
-		it("creates an user", function(done){
-			request(app)
-				.post("/api/v1/users")
-				.set("App-Access-Token", user.access_token)
-				.send({
+			models.Company.create({
+				name: "Aion"
+			}).then(function(_company){
+				company = _company;
+				var _userB =  models.User.build({
 					first_name: "Flora",
 			    last_name: "Dorea",
 			    email: "test@test.com",
 			    role: "manager",
 			    password: "test"
-				}).expect(201, done);
+				});
+				company.addUser(_userB).then(function(_user){
+					user = _user;
+					done(null);
+				});
+			});
+
+		});
+
+	});
+
+	context("#GET", function(){
+
+		testNoAuthenticetedUser("/api/v1/users", "get");
+
+		it("gets users list with access_token", function(done){
+			request
+				.get("/api/v1/users")
+				.set("App-Access-Token", user.access_token)
+				.expect(200, done);
+		});
+
+	});
+
+	context("#POST", function(){
+
+		testNoAuthenticetedUser("/api/v1/users", "post");
+
+		it("creates a new user", function(done){
+			request
+				.post("/api/v1/users")
+				.set("App-Access-Token", user.access_token)
+				.send(managerUser).expect(201, done);
 		});
 
 	});

0efd6903fde7912097be09fbb9651765988b4ee7
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-05-08 11:09:57 -0300

    fix users test

--- test/routes/users.js
+++ test/routes/users.js
@@ -4,18 +4,20 @@ var models = require("../../models");
 var testNoAuthUser = require("../utils/test_no_authenticated_user");
 var request = require('supertest')(app);
 
-describe('User root routes', function(){
-
-	var company;
-	var user;
-
-	var managerUser = {
+function newUserParams(email){
+	return {
 		first_name: "Flora",
     last_name: "Dorea",
-    email: "manager@test.com",
+    email: email,
     role: "manager",
     password: "test"
-	};
+	}
+}
+
+describe('Users routes', function(){
+
+	var company;
+	var loggedUser;
 
 	before(function(done){
 
@@ -25,15 +27,9 @@ describe('User root routes', function(){
 				name: "Aion"
 			}).then(function(_company){
 				company = _company;
-				var _userB =  models.User.build({
-					first_name: "Flora",
-			    last_name: "Dorea",
-			    email: "test@test.com",
-			    role: "manager",
-			    password: "test"
-				});
+				var _userB = models.User.build(newUserParams("master@test.com"));
 				company.addUser(_userB).then(function(_user){
-					user = _user;
+					loggedUser = _user;
 					done(null);
 				});
 			});
@@ -42,48 +38,82 @@ describe('User root routes', function(){
 
 	});
 
-	context("#GET", function(){
+	context("/api/v1/users", function(){
 
-		testNoAuthUser("/api/v1/users", "get");
+		context("#GET", function(){
 
-		it("gets users list with access_token", function(done){
-			request
-				.get("/api/v1/users")
-				.set("App-Access-Token", user.access_token)
-				.expect(200, done);
+			testNoAuthUser("/api/v1/users", "get");
+
+			it("gets users list with access_token", function(done){
+				request
+					.get("/api/v1/users")
+					.set("App-Access-Token", loggedUser.access_token)
+					.expect("Content-Type", "application/json")
+					.expect(200, done);
+			});
+
+		});
+
+		context("#POST", function(){
+
+			testNoAuthUser("/api/v1/users", "post");
+
+			it("creates a new user", function(done){
+				request
+					.post("/api/v1/users")
+					.set("App-Access-Token", loggedUser.access_token)
+					.send(newUserParams("manager_1@test.com"))
+					.expect("Content-Type", "application/json")
+					.expect(201, done);
+			});
+
+			it("cant create users with the same email", function(done){
+				
+				var _params = newUserParams("manager_2@test.com")
+
+				var _user = models.User.build(_params);
+				company.addUser(_user);
+				
+				request
+					.post("/api/v1/users")
+					.set("App-Access-Token", loggedUser.access_token)
+					.send(_params)
+					.expect("Content-Type", "application/json")
+					.expect(400, done);
+			})
+
 		});
 
 	});
 
-	context("#POST", function(){
+	context("/api/v1/users/:user_id", function(){
 
-		testNoAuthUser("/api/v1/users", "post");
+		context("#GET", function(){
 
-		it("creates a new user", function(done){
-			request
-				.post("/api/v1/users")
-				.set("App-Access-Token", user.access_token)
-				.send(managerUser).expect(201, done);
+			testNoAuthUser("/api/v1/users/1", "get");
+
+			it("shows a given user", function(done){
+				var _params = newUserParams("manager_3@test.com")
+				var _user = models.User.build(_params);
+				company.addUser(_user)
+					.then(function(user){
+						return user.id
+					}).then(function(userId){
+						request
+							.get("/api/v1/users/" + userId)
+							.set("App-Access-Token", loggedUser.access_token)
+							.expect("Content-Type", "application/json")
+							.expect(200)
+							.end(function(err, res){
+								if(err) return done(err);
+								expect(res.body).to.not.include.keys("password");
+								done();
+							});
+					});
+				
+			});
+
 		});
-
-		it("cant create users with the same email", function(done){
-			
-			var _params = {
-				first_name: "Flora",
-		    last_name: "Dorea",
-		    email: "manager@test2.com",
-		    role: "manager",
-		    password: "test"
-			};
-
-			var _user = models.User.build(_params);
-			company.addUser(_user);
-			
-			request
-				.post("/api/v1/users")
-				.set("App-Access-Token", user.access_token)
-				.send(_params).expect(400, done);
-		})
 
 	});
 
